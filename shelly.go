6fcbccb3ef1c0e7a8c8e03fcf90c309a84ad47e8
Matias <matias.j4rv3np44@gmail.com>
2023-02-15 22:33:46 +0200

    Add setSwitch-function

--- shelly.go
+++ shelly.go
@@ -6,10 +6,17 @@ import(
   "io/ioutil"
   "encoding/json"
   "log"
+  "strconv"
+  "errors"
+  "time"
   )
 
+
+const SWITCH_MAX_ON_TIME = 3600*3;
+
+
 //return (temp,errorcode)
-func getTemperature()(float64, error){
+func getTemperature(client http.Client)(float64, error){
   defer func() {
 		if err := recover(); err != nil {
 			log.Println("panic occurred:", err)
@@ -17,7 +24,7 @@ func getTemperature()(float64, error){
 	}()
 
 
-  resp, err := http.Get("https://api.openweathermap.org/data/2.5/forecast?LOCATION&cnt=8&units=metric&appid=WEATHER_TOKEN")
+  resp, err := client.Get("https://api.openweathermap.org/data/2.5/forecast?LOCATION&cnt=8&units=metric&appid=WEATHER_TOKEN")
   if err != nil {
     return 0,err;
   }
@@ -40,7 +47,7 @@ func getTemperature()(float64, error){
   var temperature float64 = 0.0;
   for i:= range list{
     listItem := list[i].(map[string]interface{})
-    main := listItem["min"].(map[string]interface{})
+    main := listItem["main"].(map[string]interface{})
     temperature += main["temp"].(float64);
   }
 
@@ -49,15 +56,38 @@ func getTemperature()(float64, error){
 }
 
 
+func setSwitch(id uint8, value bool, client http.Client)error{
+  var request string;
+  if value == true{
+    request = "SHELLY_ADDRESS/rpc/Switch.Set?id="+strconv.Itoa(int(id))+"&on=true&toggle_after="+strconv.Itoa(int(SWITCH_MAX_ON_TIME))
+  }else{
+    request = "SHELLY_ADDRESS/rpc/Switch.Set?id=" +strconv.Itoa(int(id))+"&on=false"
+  }
+
+  resp, err := client.Get(request)
+  if err != nil {
+    return err;
+  }
+  if(resp.StatusCode != 200){
+    return errors.New(resp.Status);
+  }
+  return nil;
+}
+
+
+
 func main() {
+  client := http.Client{
+    Timeout: 10 * time.Second,
+  }
 
-
-  resp, err:= getTemperature();
+  resp, err:= getTemperature(client);
   if(err == nil){
     fmt.Println(resp);
   }
 
-
-
-
+  err=setSwitch(1,false, client);
+  if(err != nil){
+    fmt.Println(err);
+  }
 }
